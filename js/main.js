0fd8ebcd78d42116b7607a6ca0cf91feba4d882c
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2022-07-29 06:08:32 -0800

    Remove unused sections, rearrange others.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-//Get the list of items
-var items;
-webRequest("GET","http://localhost/badURI",function(data){items = data;},"JSON");
+// Get the list of items
+// var items;
+// webRequest("GET","http://localhost/badURI",function(data){items = data;},"JSON");
 
-//Product and corp pages: display all properties as a list.
-//Indexes: display all products or corps in a grid
-for (let item of items) {
-	addElement("table",$innerText,$elementClass,$elementType,$elementStyle,$href,$onChange,$onClick,$contentEditable,$attributeType,$attributeAction,$elementId)
-}
+// Product and corp pages: display all properties as a list.
+// Indexes: display all products or corps in a grid
+// for (let item of items) {
+	//addElement("table",$innerText,$elementClass,$elementType,$elementStyle,$href,$onChange,$onClick,$contentEditable,$attributeType,$attributeAction,$elementId)
+//  }
